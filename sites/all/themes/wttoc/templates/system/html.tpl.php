f4d52a536cb41e65f8ba40f30cef9465f1de9fca
Luis Galeas <galeaspablo@hotmail.com>
2016-02-22 14:30:35 -0500

    norton meta tag, css for links in basic pages, and blog pages

--- sites/all/themes/wttoc/templates/system/html.tpl.php
+++ sites/all/themes/wttoc/templates/system/html.tpl.php
@@ -72,19 +72,10 @@
   <meta name="msapplication-TileColor" content="#da532c">
   <meta name="msapplication-config" content="/sites/all/themes/wttoc/images/favicons/browserconfig.xml">
   <meta name="theme-color" content="#ffffff">
+  <meta name="norton-safeweb-site-verification" content="a7nq0you59682nzihu-trns1ttee-yt589xijo-sokd4mrvtwl8-75xw5rmbp1qcbuh3evff0cso1kx0qkprywnrx3y9o8kudx0r33b4ly9r7kubr1qj-oal5r4d7wqd" />
   <!--END FAVICON SECTION -->
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-53644310-1', 'auto');
-    ga('send', 'pageview');
-
-  </script>
 </head>
 <body<?php print $attributes;?>>
   <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
